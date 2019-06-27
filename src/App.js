83511f0ff2f89ffe7f4981304245ec691ddc8e5f
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-06-27 14:08:16 -0400

    added snap + unit tests, table conditional update, http cancel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
 import "./App.css";
 
 // url constants
 const DEFAULT_QUERY = "redux";
-const DEFAULT_HPP = "100";
-const DEFAULT_TAGS = 'story'
+const DEFAULT_HPP = "20";
+const DEFAULT_TAGS = "story";
 
 const PATH_BASE = "https://hn.algolia.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
 const PARAM_PAGE = "page=";
 const PARAM_HPP = "hitsPerPage=";
-const PARAM_TAGS = "tags="
+const PARAM_TAGS = "tags=";
 
 // styles
 const largeColumn = {
@@ -29,18 +31,27 @@ const smallColumn = {
 class App extends Component {
   state = {
     results: null,
-    searchKey: '',
+    searchKey: "",
     searchTerm: DEFAULT_QUERY,
     error: null
   };
 
+  source = axios.CancelToken.source();
+
   fetchSearchTopStories = (searchTerm, page = 0) => {
-    fetch(
-      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}&${PARAM_TAGS}${DEFAULT_TAGS}`
-    )
-      .then(res => res.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => this.setState({ error }));
+    axios
+      .get(
+        `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}&${PARAM_TAGS}${DEFAULT_TAGS}`,
+        { cancelToken: this.source.token }
+      )
+      .then(result => this.setSearchTopStories(result.data))
+      .catch(error => {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled", error.message);
+        } else {
+          this.setState({ error });
+        }
+      });
   };
 
   setSearchTopStories = result => {
@@ -48,18 +59,18 @@ class App extends Component {
     const { searchKey, results } = this.state;
 
     // check if there are old hits
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].hits 
-      : [];
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
 
     // spread all hits together
     const updatedHits = [...oldHits, ...hits];
 
-    this.setState({ 
+    this.setState({
       results: {
         ...results,
         [searchKey]: { hits: updatedHits, page }
-      } });
+      }
+    });
   };
 
   needsToSearchTopStories = searchTerm => !this.state.results[searchTerm];
@@ -94,19 +105,11 @@ class App extends Component {
   };
 
   render() {
-    const { 
-      searchTerm, 
-      searchKey, 
-      results,
-      error
-    } = this.state;
-    const page = (results && results[searchKey] && results[searchKey].page) || 0;
-    const list = (
-      results &&
-      results[searchKey] &&
-      results[searchKey].hits
-    ) || [];
-
+    const { searchTerm, searchKey, results, error } = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="page">
         <div className="interactions">
@@ -118,15 +121,13 @@ class App extends Component {
             Search
           </Search>
         </div>
-        {error 
-          ? (
-            <div className="interactions">
-              <p>Something went wrong.</p>
-            </div>
-          ) 
-          : (
-            <Table list={list} onDismiss={this.onDismiss} />
-          ) }
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
+        )}
         <div className="interactions">
           <Button
             onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
@@ -143,39 +144,80 @@ class App extends Component {
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
+
+  componentWillUnmount() {
+    // cancel axios requests
+    this.source.cancel("Component unmounting.");
+  }
 }
 
-const Search = ({ searchTerm, onChange, onSubmit, children }) => (
-  <form onSubmit={onSubmit}>
-    <input type="text" value={searchTerm} onChange={onChange} />
-    <button type="submit">{children}</button>
-  </form>
-);
+const Search = ({ value, onChange, onSubmit, children }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange} />
+      <button type="submit">{children}</button>
+    </form>
+  );
+};
 
-const Table = ({ list, onDismiss }) => (
-  <div className="table">
-    {list.map(item => {
-      return (
-        <div key={item.objectID} className="table-row">
-          <span style={largeColumn}>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span style={mediumColumn}>{item.author}</span>
-          <span style={smallColumn}>{item.num_comments}</span>
-          <span style={smallColumn}>{item.points}</span>
-          <span style={smallColumn}>
-            <Button
-              onClick={() => onDismiss(item.objectID)}
-              className="button-inline"
-            >
-              Dismiss
-            </Button>
-          </span>
-        </div>
-      );
-    })}
-  </div>
-);
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node
+};
+
+class Table extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    // prevent table from rerendering all the time
+    if (this.props.list === nextProps.list) {
+      console.log(false);
+      return false;
+    }
+    console.log(true);
+    return true;
+  }
+  render() {
+    const { list, onDismiss } = this.props;
+    return (
+      <div className="table">
+        {list.map(item => {
+          return (
+            <div key={item.objectID} className="table-row">
+              <span style={largeColumn}>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span style={mediumColumn}>{item.author}</span>
+              <span style={smallColumn}>{item.num_comments}</span>
+              <span style={smallColumn}>{item.points}</span>
+              <span style={smallColumn}>
+                <Button
+                  onClick={() => onDismiss(item.objectID)}
+                  className="button-inline"
+                >
+                  Dismiss
+                </Button>
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
 
 const Button = ({ onClick, className = "", children }) => (
   <button onClick={onClick} className={className} type="button">
@@ -183,4 +225,12 @@ const Button = ({ onClick, className = "", children }) => (
   </button>
 );
 
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
 export default App;
+
+export { Button, Search, Table };
